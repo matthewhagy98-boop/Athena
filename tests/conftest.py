f4dffec7da813e57a2b0cb9995e33129abba0428
Matt Hagy <matthewhagy98@gmail.com>
2026-07-03 09:46:32 -0500

    feat: add database models and initial migration

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 from sqlalchemy import text
+from sqlalchemy.orm import Session
 
 from evidence_engine.db.session import engine
 
@@ -8,3 +9,14 @@ from evidence_engine.db.session import engine
 def db_connection():
     with engine.connect() as conn:
         yield conn
+
+
+@pytest.fixture
+def db_session():
+    connection = engine.connect()
+    transaction = connection.begin()
+    session = Session(bind=connection)
+    yield session
+    session.close()
+    transaction.rollback()
+    connection.close()
